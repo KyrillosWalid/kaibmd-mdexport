c798f925fa3da2a64f158b5fab07fed3397ea40a
Kyrillos Walid <kokadev13@gmail.com>
2020-01-01 16:53:11 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# kaibmd-mdexport
+#### This project is under development and may not be reliable.
+------------------
+## Convert md files to :
+- #### HTML
+- #### PDF 
+- #### PNG
+- #### JPEG
+#### And many formats to be added later
+
+## Some additions will be added to md :
+- #### Draw math equations
+- #### Flow mapping
+#### And many additions to be added later
+
+#### This project can be considered as a modified version of the md format.
+## (kaib-md)
---
+++ main.nim
@@ -0,0 +1,59 @@
+import strutils
+
+proc deletesign(s: string, sn: int): string = 
+    var fstr: string
+    var slen = s.len()
+    for i in sn..slen-1:
+        fstr = fstr & s[i]
+    return fstr
+
+var f: File
+var filename: string = "file.md"
+let hf = open("index.html", fmAppend)
+hf.write("""
+<html>
+<head>
+<meta charset = "UTF-8">
+<title>$1</title>
+</head>
+<body>
+""" % [filename])
+if open(f, filename): 
+    try:
+        while true:
+            var line = f.readLine()
+            var aline = line.split(' ')
+            var fline: string = aline[0]
+            case fline:
+                of "#":
+                    hf.write("<h1>")
+                    var ffline = deletesign(line,1)
+                    hf.write("$1 </h1>\n" % [ffline])
+                of "##":
+                    hf.write("<h2>")
+                    var ffline = deletesign(line,2)
+                    hf.write("$1 </h2>\n" % [ffline])
+                of "###":
+                    hf.write("<h3>")
+                    var ffline = deletesign(line,3)
+                    hf.write("$1 </h3>\n" % [ffline])
+                of "####":
+                    hf.write("<h4>")
+                    var ffline = deletesign(line,4)
+                    hf.write("$1 </h4>\n" % [ffline])
+                of "#####":
+                    hf.write("<h5>")
+                    var ffline = deletesign(line,5)
+                    hf.write("$1 </h5>\n" % [ffline])
+                of "######":
+                    hf.write("<h6>")
+                    var ffline = deletesign(line,6)
+                    hf.write("$1 </h6>\n" % [ffline])
+    except EOFError:
+        echo "Done!"
+
+hf.write("""
+</body>
+</html>
+""")    
+    
